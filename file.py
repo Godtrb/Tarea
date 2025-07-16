f88e0e366efa314fa921e35f0e95f7e1a818fe3a
GodT <Godtrb@Gmail.com>
2025-07-15 22:06:41 -0600

    pluh

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
 from tokenize import Double
 
-estudiantes={}
+productos={}
 cantidad=int(input(f"\nIngrese la cantidad de estudiantes: "))
 for i in range(cantidad):
     print(f"\n producto #{i+1}:")
@@ -9,14 +9,14 @@ for i in range(cantidad):
         if codigo not in estudiantes:
 
                 nombre=input("Ingrese el nombre del producto: ")
-                precio=float(input("Ingrese el precio: "))
+                precio=float(input("Ingrese el precio: Q"))
 
                 if (precio>0):
                     categoria = input("Ingrese la categoria del producto: ")
                     talla = input("Ingrese la talla del producto: ")
                     cantidad = int(input("Ingrese la cantidad que se agregara del producto: "))
                     if cantidad>0:
-                        estudiantes[codigo] = {
+                        productos[codigo] = {
                             "nombre": nombre,
                             "precio": precio,
                             "categoria": categoria,
@@ -40,12 +40,13 @@ for i in range(cantidad):
 
 
 
-print("\nListe de estudiantes")
-for carnet,datos in estudiantes.items():
-    print(f"Carnet: {carnet}")
-    print(f"Nombre: {datos["nombre"]}")
-    print(f"Edad: {datos["edad"]}")
-    print(f"Carrera: {datos["carrera"]}")
+print("\nListe de productos")
+for Codigo in productos.items():
+    print(f"Codigo: {Codigo}")
+    print(f"Nombre: {Codigo["nombre"]}")
+    print(f"Precio: {Codigo["precio"]}")
+    print(f"Categoria: {Codigo["categoria"]}")
+    print(f"Talla: {Codigo["talla"]}")
 
 print(" Buscar estudiante")
 ado=input("Ingrese el carnet: ")
